e7df2c78f6c6d422d6a7de2ce243c9464cb8733f
Dmitry Ivanov <id.ajantis@gmail.com>
2015-10-28 17:51:47 +0100

    Merge two CLI options together for configuring target proxied host

--- src/main/scala/io/dmitryivanov/tcpfrontman/config/CliArgParser.scala
+++ src/main/scala/io/dmitryivanov/tcpfrontman/config/CliArgParser.scala
@@ -1,6 +1,11 @@
 package io.dmitryivanov.tcpfrontman.config
 
+import java.net.URL
+import scopt.Read
+
 trait CliArgParser { this: DefaultConfigs =>
+
+  import CliArgParser._
 
   private[config] def createParser = new scopt.OptionParser[ServerConfig]("tcp-frontman") {
     private[this] def portValidation: Int => Either[String, Unit] = { value =>
@@ -16,13 +21,11 @@ trait CliArgParser { this: DefaultConfigs =>
       config.copy(bindPort = value)
     } validate { portValidation } text s"Port that the tcp proxy should run on. [Default: $DefaultBindPort]"
 
-    opt[String]('h', "target-host") valueName "<host>" action { (value, config) =>
-      config.copy(remoteHost = value)
-    } text s"Host or IP address of the server the proxy will target. [Default: $DefaultRemoteHost]"
+    opt[URL]('t', "target") valueName "<host>" action { (value, config) =>
+      val port = if (value.getPort != -1) value.getPort else getDefaultPort(value.getProtocol)
 
-    opt[Int]('p', "target-port") valueName "<port>" action { (value, config) =>
-      config.copy(remotePort = value)
-    } validate { portValidation } text s"Port of the server the proxy will target. [Default: $DefaultRemotePort]"
+      config.copy(remoteHost = value.getHost, remotePort = port)
+    } text s"Location of the server the proxy will target. [Default: $DefaultRemoteHost:$DefaultRemotePort]"
 
     help("help") text "Prints this usage text"
   }
@@ -30,4 +33,18 @@ trait CliArgParser { this: DefaultConfigs =>
   def parse(args: Array[String]): Option[ServerConfig] = {
     createParser.parse(args, ServerConfig(bindPort = DefaultBindPort, remoteHost = DefaultRemoteHost, remotePort = DefaultRemotePort))
   }
+
+  private def getDefaultPort(schema: String): Int = {
+    schema match {
+      case "https" => 443
+      case _       => 80
+    }
+  }
+
+  private def isSsl(schema: String): Boolean = schema == "https"
+}
+
+object CliArgParser {
+  import scopt.Read._
+  implicit val urlRead: Read[URL] = reads { value => new URL(value) }
 }
